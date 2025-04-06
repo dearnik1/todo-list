e2b388d1250a7f5858af2e8806f274755b119956
dearnik1 <nik.khomenko@gmail.com>
2025-04-05 23:41:51 -0500

    focus on submit

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,13 +1,20 @@
-function TodoForm() {
+import { useRef } from 'react'
+
+function TodoForm({ onAddTodo }) {
+    const todoTitleInput = useRef()
+    
     function handleAddTodo(event) {
         event.preventDefault()
-        console.dir(event.target)
+        const title = event.target.title.value
+        onAddTodo(title)
+        event.target.title.value = ""
+        todoTitleInput.current.focus()
     }
     
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Todo</label>
-            <input type="text" id="todoTitle" name="title" />
+            <input type="text" id="todoTitle" name="title" ref={todoTitleInput} />
             <button>Add Todo</button>
         </form>
     )
