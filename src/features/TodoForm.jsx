82c1c6e77e035a4650da57c05cf8574c41407998
dearnik1 <nik.khomenko@gmail.com>
2025-04-24 00:51:48 -0500

    toggle between text and input field

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react'
+import TextInputWithLabel from '../shared/TextInputWithLabel'
 
 function TodoForm({ onAddTodo }) {
     const [workingTodoTitle, setWorkingTodoTitle] = useState('')
@@ -13,11 +14,9 @@ function TodoForm({ onAddTodo }) {
     
     return (
         <form onSubmit={handleAddTodo}>
-            <label htmlFor="todoTitle">Todo</label>
-            <input 
-                type="text" 
-                id="todoTitle" 
-                name="title" 
+            <TextInputWithLabel
+                elementId="todoTitle"
+                label="Todo"
                 ref={todoTitleInput}
                 value={workingTodoTitle}
                 onChange={(event) => setWorkingTodoTitle(event.target.value)}
