1a6f08ab2d7474ac800617fed1e34905730cce84
dearnik1 <nik.khomenko@gmail.com>
2025-04-24 00:39:07 -0500

    changed project structure

---
+++ src/features/TodoForm.jsx
@@ -0,0 +1,30 @@
+import { useRef, useState } from 'react'
+
+function TodoForm({ onAddTodo }) {
+    const [workingTodoTitle, setWorkingTodoTitle] = useState('')
+    const todoTitleInput = useRef()
+    
+    function handleAddTodo(event) {
+        event.preventDefault()
+        onAddTodo(workingTodoTitle)
+        setWorkingTodoTitle('')
+        todoTitleInput.current.focus()
+    }
+    
+    return (
+        <form onSubmit={handleAddTodo}>
+            <label htmlFor="todoTitle">Todo</label>
+            <input 
+                type="text" 
+                id="todoTitle" 
+                name="title" 
+                ref={todoTitleInput}
+                value={workingTodoTitle}
+                onChange={(event) => setWorkingTodoTitle(event.target.value)}
+            />
+            <button disabled={workingTodoTitle === ''}>Add Todo</button>
+        </form>
+    )
+}
+
+export default TodoForm
