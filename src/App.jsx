caf66da06f7d31ebff180398a811b6435868f550
dearnik1 <nik.khomenko@gmail.com>
2025-05-12 23:55:35 -0500

    display error messages

--- src/App.jsx
+++ src/App.jsx
@@ -111,14 +111,57 @@ function App() {
     setTodoList(updatedTodos)
   }
   
-  const updateTodo = (editedTodo) => {
-    const updatedTodos = todoList.map((todo) => {
-      if (todo.id === editedTodo.id) {
-        return { ...editedTodo }
+  const updateTodo = async (editedTodo) => {
+    const originalTodo = todoList.find((todo) => todo.id === editedTodo.id);
+    
+    const payload = {
+      records: [
+        {
+          id: editedTodo.id,
+          fields: {
+            title: editedTodo.title,
+            isCompleted: editedTodo.isCompleted,
+          },
+        },
+      ],
+    };
+
+    const options = {
+      method: 'PATCH',
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    try {
+      setIsSaving(true);
+      const resp = await fetch(url, options);
+      if (!resp.ok) {
+        throw new Error(resp.message);
       }
-      return todo
-    })
-    setTodoList(updatedTodos)
+
+      const updatedTodos = todoList.map((todo) => {
+        if (todo.id === editedTodo.id) {
+          return { ...editedTodo };
+        }
+        return todo;
+      });
+      setTodoList(updatedTodos);
+    } catch (error) {
+      console.error(error);
+      setErrorMessage(`${error.message}. Reverting todo...`);
+      const revertedTodos = todoList.map((todo) => {
+        if (todo.id === originalTodo.id) {
+          return { ...originalTodo };
+        }
+        return todo;
+      });
+      setTodoList([...revertedTodos]);
+    } finally {
+      setIsSaving(false);
+    }
   }
   
   return (
