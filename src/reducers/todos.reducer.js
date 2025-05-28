387df5ac2a43d0ad3b7ced475c710b169e7e012e
dearnik1 <nik.khomenko@gmail.com>
2025-05-28 00:13:22 -0500

    Duplicate State Logic to Reducer

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -25,46 +25,91 @@ const initialState = {
 };
 
 function reducer(state = initialState, action) {
+    let fetchedTodos, savedTodo, updatedTodos, updatedState, completedTodos;
+
     switch (action.type) {
         case actions.fetchTodos:
             return {
                 ...state,
+                isLoading: true
             };
         case actions.loadTodos:
+            fetchedTodos = action.records.map(record => {
+                const todo = {
+                    id: record.id,
+                    ...record.fields
+                };
+                if (!todo.isCompleted) {
+                    todo.isCompleted = false;
+                }
+                return todo;
+            });
             return {
                 ...state,
+                todoList: fetchedTodos,
+                isLoading: false
             };
         case actions.setLoadError:
             return {
                 ...state,
+                errorMessage: action.error.message,
+                isLoading: false
             };
         case actions.startRequest:
             return {
                 ...state,
+                isSaving: true
             };
         case actions.addTodo:
+            savedTodo = {
+                id: action.records[0].id,
+                ...action.records[0].fields
+            };
+            if (!action.records[0].fields.isCompleted) {
+                savedTodo.isCompleted = false;
+            }
             return {
                 ...state,
+                todoList: [...state.todoList, savedTodo],
+                isSaving: false
             };
         case actions.endRequest:
             return {
                 ...state,
-            };
-        case actions.updateTodo:
-            return {
-                ...state,
-            };
-        case actions.completeTodo:
-            return {
-                ...state,
+                isLoading: false,
+                isSaving: false
             };
         case actions.revertTodo:
+        case actions.updateTodo:
+            updatedTodos = state.todoList.map(todo => {
+                if (todo.id === action.editedTodo.id) {
+                    return { ...action.editedTodo };
+                }
+                return todo;
+            });
+            updatedState = {
+                ...state,
+                todoList: updatedTodos
+            };
+            if (action.error) {
+                updatedState.errorMessage = action.error.message;
+            }
+            return updatedState;
+        case actions.completeTodo:
+            completedTodos = state.todoList.map(todo => {
+                if (todo.id === action.id) {
+                    return { ...todo, isCompleted: true };
+                }
+                return todo;
+            });
             return {
                 ...state,
+                todoList: completedTodos
             };
         case actions.clearError:
             return {
                 ...state,
+                errorMessage: ""
             };
         default:
             return state;
